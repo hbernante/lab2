66f1d5f8c4bbfe605b259562b1728cdd84871b51
hbernante <119903632+hbernante@users.noreply.github.com>
2023-02-03 09:17:17 +0800

    Week 9

---
+++ week9/website.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+
+<meta name="viewport" content="with=device-width, initial-scale=1.0">
+<title>Hanzel Bernante WEBPROG</title>
+
+<style>
+.pic {
+width: 10%;
+length: 10%;
+display: block;
+position: relative;
+margin-left: auto;
+margin-right: auto;
+}
+</style>
+
+<style>
+p.dotted {border-style: dotted;}
+p.dashed {border-style: dashed;}
+p.solid {border-style: solid;}
+p.double {border-style: double;}
+p.groove {border-style: groove;}
+p.ridge {border-style: ridge;}
+p.inset {border-style: inset;}
+p.outset {border-style: outset;}
+p.none {border-style: none;}
+p.hidden {border-style: hidden;}
+p.mix {border-style: dotted dashed solid double;}
+</style>
+
+<style>
+<!--body {
+	background-image: url("zelwebsite1.jpg");
+	color: white;
+	font-family: courier;
+	background-repeat: repeat-y;
+}-->
+
+a {
+	color: white;
+}
+</style>
+
+<style>
+.container {
+width: 75%;
+margin: 0 auto;
+}
+
+header {
+border-style: solid;
+background: #84afd6;
+}
+
+header::after {
+content: '';
+display: table;
+clear: both;
+}
+
+nav {
+display: flex;
+float: right;
+align-tems: center;
+}
+
+nav ul {
+margin: 0;
+padding: 0;
+list-style: none;
+}
+
+nav li {
+display: inline-block;
+margin-left: 50px;
+padding-top: 15px;
+
+position: relative;
+}
+
+nav a {
+color: darkblue;
+text-decoration: none;
+text-transform: uppercase;
+font-size: 14px;
+}
+
+nav a:hover {
+color: white;
+}
+
+nav a::before {
+content: '';
+display: block;
+height: 5px;
+width: 100%;
+background-color: #444;
+
+position: absolute;
+top: 0;
+width: 0%;
+
+transition: all ease-in-out 500ms;
+}
+
+nav a:hover::before {
+width: 100%;
+}
+
+.text-box {
+width: 90%
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate (-50%,-50%);
+text-align: center;
+}
+
+.text-box h1 {
+font-size: 62px;
+}
+
+.text-box h4 {
+font-size: 22px;
+display: inline-block;
+border: 2px solid darkblue
+}
+
+.text-box h2 {
+font-size: 42px;
+}
+
+.text-box p {
+margin: 10px 0 40px;
+font-size: 14px;
+color: #fff;
+}
+
+.hero-btn {
+display: inline-block;
+text-decoration: none;
+color: fff;
+border: 1px solid #fff;
+padding 12px 34px;
+font-size: 13px
+background: transparent;
+position: relative;
+cursor: pointer;
+}
+
+.hero-btn:hover {
+border: 1px solid #f44336;
+background: #f44336
+transition: 1s;
+}
+
+.course{
+width: 80;
+margin: auto;
+text-align: center;
+padding-top:100px;
+}
+
+.text box h4 {
+border: 1px solid #f44336;
+}
+
+</style>
+
+<b>Welcome to Hanzel Bernante's Website
+<header>
+<div class="container">
+
+<nav>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Lessons</a></li>
+<li><a href="#">Socials</a></li>
+</ul>
+</nav>
+
+</div>
+</header>
+
+<body>
+<div class="text-box">
+<h1 style="border: 2px solid white">Welcome USER to my Personal Website</h1>
+<h2> This is a website about myself</h2>
+<h3>About Me:</h3>
+<h4>
+Name: HANZEL
+<br>School: APC (Asia Pacific College
+<br>Section: BSIT-MI211
+<br>Sports: Table Tennis and Basketball
+<br>Hobbies: Working Out, Playing the Computer
+</h4>
+<p>______________________ </p>
+<a href="" class="hero-btn">Sample Link here</a>
+</div>
+
+</body>
+
+<!------ Course ------>
+<section calss="course">
+<h1> Start of another section </h1>
+<p>anlag ng ng aking imong laptop -.-</p>
+</section>
+
+
+<body>
+<br>
+<img style="border: 2px solid white" src="hanzel.jpg" alt="Hanzel's Picture" class="pic">
+<br>
+
+<h4> 
+Name: Hanzel
+<br>Age: 20
+<br>Birthday: January 8, 2003
+<br>Hobbies: Computer, Watching Movies, Table Tennis, and Working Out</br>
+</h4>
+	
+
+
+<i><p style="font-size:160%; border: 2px solid black; background-color: lightblue; text-align:center; color: darkblue;">This is the list of <ins>W3School Courses</ins> that I have Finished</p></i>
+<ul>
+<li><a href="https://www.w3schools.com/html/default.asp">Lesson 1.1 - Home</a></li>
+<li><a href="https://www.w3schools.com/html/html_intro.asp">Lesson 1.2 - Introduction</a></li>
+<li><a href="https://www.w3schools.com/html/html_editors.asp">Lesson 1.3 - Editors</a></li>
+<li><a href="https://www.w3schools.com/html/html_basic.asp">Lesson 1.4 - Basic</a></li>
+<li><a href="https://www.w3schools.com/html/html_elements.asp">Lesson 1.5 - Elements</a></li>
+<li><a href="https://www.w3schools.com/html/html_attributes.asp">Lesson 1.6 - Attributes</a></li>
+<li><a href="https://www.w3schools.com/html/html_comments.asp">Lesson 1.7 - Comments</a></li>
+<li><a href="https://www.w3schools.com/html/html_headings.asp">Lesson 2.1 - Headings</a></li>
+<li><a href="https://www.w3schools.com/html/html_paragraphs.asp">Lesson 2.2 - Paragraphs</a></li>
+<li><a href="https://www.w3schools.com/html/html_styles.asp">Lesson 2.3 - Styles</a></li>
+<li><a href="https://www.w3schools.com/html/html_formatting.asp">Lesson 2.4 - Formatting</a></li>
+<li><a href="https://www.w3schools.com/html/html_quotation_elements.asp">Lesson 2.5 - Quotations</a></li>
+<li><a href="https://www.w3schools.com/html/html_css.asp>Lesson 2.6 - CSS</a></li>
+<li><a href="https://www.w3schools.com/html/html_favicon.asp">Lesson 2.7 - Favicon</a></li>
+<li><a href="https://www.w3schools.com/html/html_blocks.asp">Lesson 2.8 - Block & Line</a></li>
+<li><a href="https://www.w3schools.com/html/html_classes.asp">Lesson 2.9 - Classes</a></li>
+<li><a href="https://www.w3schools.com/html/html_id.asp">Lesson 2.10 - ID</a></li>
+<li><a href="https://www.w3schools.com/html/html_filepaths.asp">Lesson 2.11 - File Paths</a></li>
+<li><a href="https://www.w3schools.com/html/html_head.asp">Lesson 2.12 - Head</a></li>
+<li><a href="https://www.w3schools.com/html/html_layout.asp">Lesson 2.13 - Layout</a></li>
+<li><a href="https://www.w3schools.com/html/html_responsive.asp">Lesson 2.14 - Responsive</a></li>
+<li><a href="https://www.w3schools.com/html/html5_semantic_elements.asp">Lesson 2.15 - Semantics</a></li>
+<li><a href="https://www.w3schools.com/html/html5_syntax.asp">Lesson 2.16 - Style Guide</a></li>
+<li><a href="https://www.w3schools.com/html/html_entities.asp">Lesson 2.17 - Entities</a></li>
+<li><a href="https://www.w3schools.com/html/html_symbols.asp">Lesson 2.18 - Symbols</a></li>
+<li><a href="https://www.w3schools.com/html/html_charset.asp">Lesson 2.19 - Charset</a></li>
+<li><a href="https://www.w3schools.com/html/html_urlencode.asp">Lesson 2.20 - URL Encode</a></li>
+<li><a href="https://www.w3schools.com/html/html_xhtml.asp">Lesson 2.21 - HTML vx XHTML</a></li>
+
+</ul>
+
+</body>
+</html> 
